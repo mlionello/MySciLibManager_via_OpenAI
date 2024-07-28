a8637dfff3ccced7fde5d1fd4387a36b611ce3c8
matteo <lionello.matteo@gmail.com>
2024-07-28 12:02:06 +0200

    bd vsc file as args

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from database import create_database_from_csv, query_metadata, get_metadata_by_id
+import argparse
 import os
 
 app = Flask(__name__)
@@ -17,8 +18,14 @@ def view(metadata_id):
     return render_template('view.html', metadata=metadata)
 
 if __name__ == '__main__':
-    csv_file = 'metadata.csv'  # Path to your CSV file
-    db_file = 'metadata.db'    # Name of the SQLite database file
+    parser = argparse.ArgumentParser(description='Run Flask web application with SQLite database created from CSV file.')
+    parser.add_argument('csv_file', type=str, help='Path to the CSV file')
+    parser.add_argument('--db_file', type=str, default='metadata.db', help='Name of the SQLite database file (default: metadata.db)')
+
+    args = parser.parse_args()
+
+    csv_file = args.csv_file
+    db_file = args.db_file
 
     if not os.path.exists(db_file):
         create_database_from_csv(csv_file, db_file)
