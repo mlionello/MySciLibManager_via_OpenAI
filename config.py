01012def3b1ebaa6a4e1cedd64b0e23db8b35bc2
Matteo Lionello <lionello.matteo@gmail.com>
2025-01-07 18:33:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
-OPENAI_API_KEY='INSERT HERE YOUR OPENAI API KEY'
-PROMPT_INIT = '''IT WORKS BETTER IF YOU GIVE SOME CONTEXT HERE'''
-PROMPT_INSTRUCTION = '''My interest is how/I am studying/....\n
-        Given the following scientific publication, please just fill the following form (no formatting):\n"'''
+OPENAI_API_KEY='INSERT HERE YOUR OPENAI API KEY' # keep it between the ' '
+
+# you can use a short introduction as prior knowledge in order to help to foccus on a problem, otherwise you can keep it clean or sue some output provided by https://consensus.app/ 
+PROMPT_INIT = '''IT WORKS BETTER IF YOU GIVE SOME CONTEXT HERE''' 
+
+# This is also helpful to focalize on a specific aspect while reading throughout the pubs
+PROMPT_INSTRUCTION = '''My interest is how/I am studying/... \n
+        Given the following scientific publication, please just fill the following form (no formatting):\n"''' ### keep this line as it is to input the instruction to the model
+
+# add the information you are looking for in the following set of labels. keep RESULT_LABELS and DB_LABELS aligned (saved for Path and Filename in DB_LABELS)
 RESULT_LABELS = [
     "Title",
     "Authors",
@@ -11,7 +17,7 @@ RESULT_LABELS = [
     "Main finding",
     "Two-sentences abstract",
     "Subtopic",
-    "What key message for my research interest",
+    "What key message for my research interest", ## ADD MORE questions (but add also one keyword in DB_LABELS for each of them)
 ]
 DB_LABELS = [
     "Path", # this is mandatory
@@ -24,5 +30,5 @@ DB_LABELS = [
     "Findings",
     "ShortAbstract",
     "Subtopic",
-    "RQ"
+    "RQ",
 ]
